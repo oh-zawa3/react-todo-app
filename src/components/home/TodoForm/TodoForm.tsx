aa593981059a0208933a6e68982432595f46b105
oh-zawa3 <oh.zawa3@gmail.com>
2023-04-27 15:32:32 +0900

    Todoリスト編集ボタン途中

--- src/components/home/TodoForm/TodoForm.tsx
+++ src/components/home/TodoForm/TodoForm.tsx
@@ -27,7 +27,7 @@ export const TodoForm = memo((props: TodoFormProps) => {
 
   /** ボタンの挙動の管理  */
   const handleClickButton = () => {
-    onClick({ text: textValue.todo, isDone: false, deadline: "" }); // deadline は仮
+    onClick({ text: textValue.todo, isDone: false, deadline: null }); // deadline は仮
     setTextValue(initialTextValue);
   };
 
--- src/types/Todo.d.ts
+++ src/types/Todo.d.ts
@@ -1,6 +1,7 @@
 export type Todo = {
+  id: number;
   text: string,
   isDone: boolean,
-  deadline: string
+  deadline?: Date | null
   // ... 他にも必要なものがあれば
 }
